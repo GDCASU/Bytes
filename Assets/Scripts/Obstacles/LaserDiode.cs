cdd8c316c862e6581d6587cd3e746928a9d9fbd4
amtrang <61100945+amtrang@users.noreply.github.com>
2022-08-17 22:51:20 -0700

    Laser Diode Prototype 2
    Added flicker option so that the laser can turn itself on and off at intervals.

--- Assets/Scripts/Obstacles/LaserDiode.cs
+++ Assets/Scripts/Obstacles/LaserDiode.cs
@@ -2,9 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider), typeof(LineRenderer))]
 public class LaserDiode : MonoBehaviour
 {
+    public float laserStartPoint = 0.0f;
+    public float laserMaxDistance = 100.0f;
     public float damage = 1.0f;
+    public bool isFlickering = false;
+    public float flickerStartDelay = 0.5f;
+    public float flickerTime = 1.0f;
+
+    private BoxCollider boxCollider;
+    private LineRenderer lineRenderer;
+    private bool isOn;
+
+    private void Awake()
+    {
+        boxCollider = this.GetComponent<BoxCollider>();
+        lineRenderer = this.GetComponent<LineRenderer>();
+    }
+
+    private void Start()
+    {
+        boxCollider.center = new Vector3(0, (laserMaxDistance / 2.0f) + laserStartPoint, 0);
+        boxCollider.size = new Vector3(0, laserMaxDistance, 0);
+        lineRenderer.SetPosition(0, new Vector3(0, laserStartPoint, 0));
+        lineRenderer.SetPosition(1, new Vector3(0, laserMaxDistance - laserStartPoint, 0));
+        if (isFlickering) InvokeRepeating("FlickerLaser", flickerStartDelay, flickerTime);
+    }
 
     private void OnTriggerStay(Collider other)
     {
@@ -12,6 +37,22 @@ public class LaserDiode : MonoBehaviour
             other.gameObject.GetComponent<Player>())
         {
             other.gameObject.GetComponent<Player>().TakeDamage(damage);
+        }
+    }
+
+    private void FlickerLaser()
+    {
+        if (isOn)
+        {
+            boxCollider.enabled = false;
+            lineRenderer.enabled = false;
+            isOn = false;
+        }
+        else
+        {
+            boxCollider.enabled = true;
+            lineRenderer.enabled = true;
+            isOn = true;
         }
     }
 }
