7c619c23984ba9be6e4e7ccd684dd44ac22f28a1
amtrang <61100945+amtrang@users.noreply.github.com>
2022-08-18 16:59:23 -0700

    Laser Diode Prototype Finished
    The laser diode extends to a max distance that can be set. Also, the laser stops at objects (except the player). You can try moving the wall in the scene manager the laser hits when playing the game.

--- Assets/Scripts/Obstacles/LaserDiode.cs
+++ Assets/Scripts/Obstacles/LaserDiode.cs
@@ -2,55 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider), typeof(LineRenderer))]
+[RequireComponent(typeof(LineRenderer))]
 public class LaserDiode : MonoBehaviour
 {
-    public float laserStartPoint = 0.0f;
+    [Header("Laser Variables")]
+    public Vector3 laserDir = Vector3.zero;
+    public float laserStartBuffer = 0.0f;
     public float laserMaxDistance = 100.0f;
     public float damage = 1.0f;
+
+    [Header("Flicker Variables")]
     public bool isFlickering = false;
     public float flickerStartDelay = 0.5f;
     public float flickerTime = 1.0f;
 
-    private BoxCollider boxCollider;
     private LineRenderer lineRenderer;
     private bool isOn;
 
     private void Awake()
     {
-        boxCollider = this.GetComponent<BoxCollider>();
         lineRenderer = this.GetComponent<LineRenderer>();
-    }
-
-    private void Start()
-    {
-        boxCollider.center = new Vector3(0, (laserMaxDistance / 2.0f) + laserStartPoint, 0);
-        boxCollider.size = new Vector3(0, laserMaxDistance, 0);
-        lineRenderer.SetPosition(0, new Vector3(0, laserStartPoint, 0));
-        lineRenderer.SetPosition(1, new Vector3(0, laserMaxDistance - laserStartPoint, 0));
+        lineRenderer.SetPosition(0, new Vector3(0, laserStartBuffer, 0));
+        lineRenderer.SetPosition(1, new Vector3(0, laserMaxDistance - laserStartBuffer, 0));
         if (isFlickering) InvokeRepeating("FlickerLaser", flickerStartDelay, flickerTime);
+        isOn = true;
     }
 
-    private void OnTriggerStay(Collider other)
+    private void FixedUpdate()
     {
-        if (other.gameObject.CompareTag("Player") &&
-            other.gameObject.GetComponent<Player>())
+        if (isOn)
         {
-            other.gameObject.GetComponent<Player>().TakeDamage(damage);
+            if (Physics.Raycast(transform.position, laserDir, out RaycastHit hit, laserMaxDistance))
+            {
+                GameObject player = hit.collider.gameObject;
+                if (player.CompareTag("Player") &&
+                    player.GetComponent<Player>())
+                {
+                    // Cause damage to the player
+                    player.GetComponent<Player>().TakeDamage(damage);
+                }
+                else
+                {
+                    // Laser extends to the object it hits
+                    lineRenderer.SetPosition(0, new Vector3(0, laserStartBuffer, 0));
+                    lineRenderer.SetPosition(1, new Vector3(0, (hit.distance - laserStartBuffer) * 2, 0));
+                }
+            }
+            else
+            {
+                // If nothing is hit, laser extends to its maximum distance that was set
+                lineRenderer.SetPosition(0, new Vector3(0, laserStartBuffer, 0));
+                lineRenderer.SetPosition(1, new Vector3(0, (laserMaxDistance - laserStartBuffer), 0));
+            }
         }
     }
 
+    /// <summary>
+    /// The laser can turn itself on and off at set intervals when isFlickering is true
+    /// </summary>
     private void FlickerLaser()
     {
         if (isOn)
         {
-            boxCollider.enabled = false;
             lineRenderer.enabled = false;
             isOn = false;
         }
         else
         {
-            boxCollider.enabled = true;
             lineRenderer.enabled = true;
             isOn = true;
         }
