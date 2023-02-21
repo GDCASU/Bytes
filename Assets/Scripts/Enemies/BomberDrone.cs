47719de0f08fa95c39690ce37c69df5c10c7d20a
amtrang <amtrang@asu.edu>
2023-02-20 23:36:27 -0700

    Planned Obstacle Avoidance, Set Bomber Vars
    Planned the drone's behavior for avoiding obstacles. Also put some variables unique for the bomber drone.

--- Assets/Scripts/Enemies/BomberDrone.cs
+++ Assets/Scripts/Enemies/BomberDrone.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 // Vertical Slice: https://docs.google.com/document/d/1CcRi0IGXlqaR201UI9EBUWjuiFyrSbZiKEtRt3kqeTI/edit#
@@ -12,9 +13,18 @@ using UnityEngine;
 /// </summary>
 public class BomberDrone : DroneBase
 {
+    [Header("Bomber Drone Variables")]
+    [Tooltip("The amount of damage to hurt the target player")]
+    [SerializeField] private float damage = 80;
+    [Tooltip("The amount of force the explosion pushes objects")]
+    [SerializeField] private float knockback = 7;
+    [Tooltip("The distance it travels after its charge attack before it returns to roaming state")]
+    [SerializeField] private float maxLaunchDistance = 15;
+
     protected override void Start()
     {
         base.Start();
+        
     }
 
     protected override void FixedUpdate()
@@ -44,7 +54,10 @@ public class BomberDrone : DroneBase
         transform.LookAt(target.transform);
 
         // Chase player
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, droneSpeed * Time.deltaTime);
 
+        // Obstacle detection starts here
+        //transform.Translate(0.1f, 0.0f, 0.0f);
     }
 
     protected override void SecondaryAction()
--- Assets/Scripts/Enemies/DroneBase.cs
+++ Assets/Scripts/Enemies/DroneBase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 /// <summary>
@@ -11,6 +12,10 @@ using UnityEngine;
 public abstract class DroneBase : MonoBehaviour
 {
     [Header("Drone Base Variables")]
+    [Tooltip("The empty game object's transform that's used for firing objects in front of the drone")]
+    [SerializeField] protected Transform firePoint;
+    [Tooltip("The tag of the object that the drone will seek")]
+    [SerializeField] protected string targetTag = string.Empty;
     [Tooltip("Speed of the drone's movement")]
     [SerializeField] protected float droneSpeed = 2.0f;
     [Tooltip("Use this activate an initial action when this drone is close to the target")]
@@ -19,13 +24,26 @@ public abstract class DroneBase : MonoBehaviour
     [SerializeField] protected float closeToTargetRadius = 5.0f;
     [Tooltip("Delay the secondary action by this many seconds")]
     [SerializeField] protected float chargeTime = 2.0f;
-    [SerializeField] protected string targetTag = string.Empty;
+    [Tooltip("The length of the raycasts that will check for obstacles")]
+    [SerializeField] protected float obstacleCheckRayLength = 4.0f;
+    [Tooltip("Separates the two raycasts that checks for obstacles with this angle in radians")]
+    [SerializeField] [Range(0.0f, 1.57f)] protected float obstacleCheckRayAngle = 1.0f;
+
+    [Header("Reverse Roaming Variables")]
+    [Tooltip("Number of seconds to rotate the drone 180 degrees")]
+    [SerializeField] private float rotateTime = 2.0f;
+    [Tooltip("Number of seconds to move the drone forward after the 180 degree rotation")]
+    [SerializeField] private float movementTime = 2.0f;
+    [Tooltip("Distance the drone checks in front of it")]
+    [SerializeField] private float distanceToObstacle = 5.0f;
 
     protected GameObject target;
+    protected bool isActing;
 
     protected virtual void Start()
     {
         target = GameObject.FindGameObjectWithTag(targetTag);
+        isActing = false;
     }
 
     protected virtual void FixedUpdate()
@@ -55,11 +73,23 @@ public abstract class DroneBase : MonoBehaviour
 
     protected abstract void SecondaryAction();
 
+    /// <summary>
+    /// Reverse the drone's direction and move back if the drone is directly in front of an obstacle
+    /// </summary>
+    protected IEnumerator ReverseDirection()
+    {
+        yield return null;
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, detectTargetRadius);
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, closeToTargetRadius);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(firePoint.position, new Vector3(-(Mathf.Sin(obstacleCheckRayAngle) * obstacleCheckRayLength), 0.0f, Mathf.Cos(obstacleCheckRayAngle) * obstacleCheckRayLength));
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(firePoint.position, new Vector3(Mathf.Sin(obstacleCheckRayAngle) * obstacleCheckRayLength, 0.0f, Mathf.Cos(obstacleCheckRayAngle) * obstacleCheckRayLength));
     }
 }
