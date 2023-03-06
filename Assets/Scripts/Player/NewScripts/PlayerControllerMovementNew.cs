3432e602a19c2b64dfd7d141759c2f791e22b525
hobbes144 <77174374+hobbes144@users.noreply.github.com>
2023-03-05 18:18:39 -0800

    First Person Controler
    Movement and Jump
    First Draft

--- Assets/Scripts/Player/NewScripts/PlayerControllerMovementNew.cs
+++ Assets/Scripts/Player/NewScripts/PlayerControllerMovementNew.cs
@@ -4,7 +4,79 @@ using UnityEngine;
 
 public partial class PlayerControllerNew
 {
+    [Header("Movement")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float groundDrag;
+    [SerializeField] float airDrag;
+    [SerializeField] Transform orientation;
 
-    
+    [SerializeField] float jumpPower;
+    [SerializeField] float jumpCooldown;
+    [SerializeField] float airMultplier;
+    bool canJump;
+
+    Vector2 moveInput;
+    float horizontalInput;
+    float verticelInput;
+
+    Vector3 moveDirection;
+
+
+    private void handleMove()
+    {
+        moveInput = _input.MoveVector;
+
+        horizontalInput = moveInput.x;
+        verticelInput = moveInput.y;
+
+        moveDirection = orientation.forward * verticelInput + orientation.right * horizontalInput;
+
+        if (grounded) { //moving on ground
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultplier, ForceMode.Force);
+        }
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else rb.drag = 0f;
+    }
+
+    private void speedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //limit velocity if needed
+        if (flatVel.magnitude> moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void handleJump()
+    {
+        if (_input.IsJumpPressed && canJump && grounded)
+        {
+            canJump = false;
+            jump();
+            Invoke(nameof(jump), jumpCooldown);
+        }
+    }
+    private void jump()
+    {
+        //reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpPower, ForceMode.Impulse);
+    }
+    private void resetJump()
+    {
+        canJump = true;
+    }
 
 }
--- Assets/Scripts/Player/NewScripts/PlayerControllerNew.cs
+++ Assets/Scripts/Player/NewScripts/PlayerControllerNew.cs
@@ -5,9 +5,31 @@ using UnityEngine;
 public partial class PlayerControllerNew : MonoBehaviour
 {
     PlayerInput _input;
+    Rigidbody rb;
+
+    [Header("Ground Check")]
+    [SerializeField] float playerHieght;
+    [SerializeField] LayerMask whatIsGround;
+    bool grounded;
 
     private void Awake()
     {
         _input = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHieght * 0.5f + 0.2f, whatIsGround);
+
+        speedControl();
+    }
+
+    private void FixedUpdate()
+    {
+        handleMove();
+        handleJump();
     }
 }
