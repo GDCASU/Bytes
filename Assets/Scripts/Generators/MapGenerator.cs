9681c2c0fc6b6b20055421c7cdc545cc7795a987
Ryan Carpenter <dev.ryan1184@gmail.com>
2023-03-08 21:54:12 -0800

    Blueprint Procedure Update

--- Assets/Scripts/Generators/MapGenerator.cs
+++ Assets/Scripts/Generators/MapGenerator.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    [Header("Grid Settings")]
-    public float cellSize = 10;
+    const int roomFaces = 6;
+
+    [Header("Settings")]
+    public float gridCellSize = 10;
+
+    [Header("Max Rooms")]
+    public int mainTrailMaxRooms = 10;
+    public int augmentationTrailMaxRooms = 1;
+    public int keycardTrailMaxRooms = 2;
+    public int trialTrailMaxRooms = 2;
+    public int bossTrailMaxRooms = 2;
+
+    [Header("Room Spawn Chances")]
+    [Range(0, 1)] public float tRoomChance = 0.4f;
+    [Range(0, 1)] public float hRoomChance = 0.5f;
+    [Range(0, 1)] public float bRoomChance = 1.0f;
+
 
     [Header("Room Prefabs")]
     public GameObject bluePrintPrefab;
@@ -16,22 +32,13 @@ public class MapGenerator : MonoBehaviour
 
     [Header("Trails")]
     public List<BlueprintRoom> masterTrail = new List<BlueprintRoom>(); // All trails combined
-    public List<BlueprintRoom> mainTrail = new List<BlueprintRoom>();
-    public List<BlueprintRoom> augmentationTrail = new List<BlueprintRoom>();
-    public List<BlueprintRoom> keycardTrail = new List<BlueprintRoom>();
-    public List<BlueprintRoom> trialTrail = new List<BlueprintRoom>();
-    public List<BlueprintRoom> bossTrail = new List<BlueprintRoom>();
+    public List<BlueprintRoom> mainTrail = new List<BlueprintRoom>(); // Trail to Boss Room
+    public List<BlueprintRoom> augmentationTrail = new List<BlueprintRoom>(); // Trail to Augmentation Room
+    public List<BlueprintRoom> keycardTrail = new List<BlueprintRoom>(); // Trail to Keycard Room
+    public List<BlueprintRoom> trialTrail = new List<BlueprintRoom>(); // Trail to Trial Room
+    public List<BlueprintRoom> bossTrail = new List<BlueprintRoom>(); // Trail to Boss Room
 
-    [Header("Conditions")]
-    
-    public int mainTrailMaxRooms = 10;
-    public int augmentationTrailMaxRooms = 1;
-    public int keycardTrailMaxRooms = 2;
-    public int trialTrailMaxRooms = 2;
-    public int bossTrailMaxRooms = 2;
-
-    int roomFaces = 6;
-    int maxRooms = 20;
+    int maxRooms = 0;
     int entrFlagIdx = 0;
     void Start()
     {
@@ -66,29 +73,29 @@ public class MapGenerator : MonoBehaviour
             tempPos = curPos;
             switch (UnityEngine.Random.Range(1, 7)) // Choosing position of next room
             {
-                case 1: tempPos += Vector3.right * cellSize; // E0 (cellSize, 0, 0) * Cell Unit Size
+                case 1: tempPos += Vector3.right * gridCellSize; // E0 (cellSize, 0, 0) * Cell Unit Size
                     entrFlagIdx = 0;
                     break;
-                case 2: tempPos += Vector3.left * cellSize; // E1 (-cellSize, 0, 0) * Cell Unit Size
+                case 2: tempPos += Vector3.left * gridCellSize; // E1 (-cellSize, 0, 0) * Cell Unit Size
                     entrFlagIdx = 1;
                     break;
-                case 3: tempPos += Vector3.forward * cellSize; // E2 (0, 0, cellSize) * Cell Unit Size
+                case 3: tempPos += Vector3.forward * gridCellSize; // E2 (0, 0, cellSize) * Cell Unit Size
                     entrFlagIdx = 2;
                     break;
-                case 4: tempPos += Vector3.back * cellSize; // E3 (0, 0, -cellSize) * Cell Unit Size
+                case 4: tempPos += Vector3.back * gridCellSize; // E3 (0, 0, -cellSize) * Cell Unit Size
                     entrFlagIdx = 3;
                     break;
-                case 5: tempPos += Vector3.up * cellSize;  // E4 (0, cellSize, 0) * Cell Unit Size
+                case 5: tempPos += Vector3.up * gridCellSize;  // E4 (0, cellSize, 0) * Cell Unit Size
                     entrFlagIdx = 4;
                     break;
-                case 6: tempPos += Vector3.down * cellSize; // E5 (0, -cellSize, 0) * Cell Unit Size
+                case 6: tempPos += Vector3.down * gridCellSize; // E5 (0, -cellSize, 0) * Cell Unit Size
                     entrFlagIdx = 5;
                     break; 
             }
 
             bool inRoomList = false;
             BlueprintRoom conflictedRoom = null;
-            foreach(BlueprintRoom room in masterTrail) // Check master trail for conflicting rooms
+            foreach(BlueprintRoom room in masterTrail) // Check master trail for colliding rooms (the temp pos is inside another designated room space)
             {
                 if (Vector3.Equals(tempPos, room.position)) // Test Failed
                 {
@@ -105,16 +112,20 @@ public class MapGenerator : MonoBehaviour
 
                 BlueprintRoom newRoom = new BlueprintRoom(curPos);
                 FlagDoorways(newRoom, curRoom, entrFlagIdx);
+                GenerateBlueprintGizmo(curPos);
 
                 curRoom = newRoom;
                 trail.Add(newRoom);
                 masterTrail.Add(newRoom);
+
+                failedAttempts = 0;
             }
 
-            if (failedAttempts > 6) // If failed backtrack
+            if (failedAttempts > 6) // If failed backtrack (very rare)
             {
                 curPos = tempPos;
                 curRoom = conflictedRoom;
+                failedAttempts = 0;
             }
         }
     }
@@ -129,7 +140,7 @@ public class MapGenerator : MonoBehaviour
         prevRoom.activeEntranceways[entrFlagIdx] = true;
     }
 
-    private void GenerateBlueprintRoom(Vector3 roomPosition)
+    private void GenerateBlueprintGizmo(Vector3 roomPosition) // Generate Gizmo for Debugging purposes 
     {
         GameObject genRoom = Instantiate(bluePrintPrefab, roomPosition, Quaternion.identity) as GameObject;
         genRoom.name = $"{bluePrintPrefab.name}";
--- Assets/Scripts/Generators/Room/BlueprintRoom.cs
+++ Assets/Scripts/Generators/Room/BlueprintRoom.cs
@@ -2,22 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.WSA;
-
-public enum RoomShape
-{
-    General,
-    Tall,
-    Hall,
-    Big,
-    Boss
-}
 
 public class BlueprintRoom // Psudo Room used for room generation algorithm
 {
     public Vector3 position;
-    public RoomShape shape;
     public bool[] activeEntranceways;
+
+    public GameObject RoomGadget;
 
     public BlueprintRoom(Vector3 postion)
     {
