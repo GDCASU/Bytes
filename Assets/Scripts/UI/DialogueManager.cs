535ddae943807f2e6b25e22f76d728cebab1f5ec
Hyzel11 <jcsmit74@asu.edu>
2023-03-31 16:21:16 -0700

    Code1
    Got to 10:53 in the video. Since he uses a start button, I still need to make it so the player reaches a certain state in the level that triggers the dialogue.
    
    Also, he uses a continue button. I need to change this to just pressing enter at some point.
    
    Lastly, Unity is currently giving me an error regarding the Dialogue Trigger script. Will look into this next time.

--- Assets/Scripts/UI/DialogueManager.cs
+++ Assets/Scripts/UI/DialogueManager.cs
@@ -1,15 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-    public Queue<string> sentences;
+    public Text nameText;
+    public Text dialogueText;
+
+
+    private Queue<string> sentences;
 
 
     void Start()
     {
         sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End");
     }
 
 }
