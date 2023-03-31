d6b8aeffe3a009905a6fdb36c05cb6c4aa64b37a
Hyzel11 <jcsmit74@asu.edu>
2023-03-31 15:08:29 -0700

    Text Box and New Script
    Fixed the text box so it appears like a regular text box, and created the Dialogue Manager script.

--- Assets/Scripts/UI/DialogueManager.cs
+++ Assets/Scripts/UI/DialogueManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Queue<string> sentences;
+
+
     void Start()
     {
-        
+        sentences = new Queue<string>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
