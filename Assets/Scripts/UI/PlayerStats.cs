b1d58b39c2216204401e21457ea707e57cfe851e
FalconKiaT <ian.fletcher778@gmail.com>
2023-03-31 15:09:20 -0700

    Optimized Code for stat changes

--- Assets/Scripts/UI/PlayerStats.cs
+++ Assets/Scripts/UI/PlayerStats.cs
@@ -4,69 +4,72 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour 
 {
+	public bool debugOn = false; //if true, prints values to console
 	readonly public int limitHealth = 500; //Limits the max amount of health you can upgrade to
 	readonly public int limitBattery = 10; //Limits the max amount of battery you can upgrade to
-	public int maxHealth; //current limit of the full health of the player, upgradable
-	public bool debugOn = false; //if true, prints values to console
-	public int health;
-	public int battery;
-	public int maxBattery; //Sets the max battery possible, can be changed to increase charges
+	public int maxBattery; //Sets the max battery possible, upgradeable
+	public int maxHealth; //current limit of the full health of the player, upgradeable
+	public int health; //Current health of the player
+	public int battery; //Current battery of the player
 	
-	//TODO: Add ammunition stats and more
+	//TODO: Add ammunition inventory and more
 	
 	public PlayerStats() 
 	{
+		maxHealth = 100;
+		maxBattery = 5;
 		health = 100;
 		battery = 5;
-		maxBattery = 5; //Starting max battery charges, upgradable
-		maxHealth = 100; 
 	}
 
 	public void takeDamage(int damage)
 	{
-		if ( health - damage < 0 ) 
+		int newHealth = health - damage;
+		if ( newHealth > 0 ) //Enough Health to tank incoming damage
 		{
-			deathEvent();
-			return;
+			health = newHealth;
+			if (debugOn) 
+			{
+				Debug.Log("Damage Taken! " + "Health = " + health.ToString());
+			}
 		}
-		health -= damage;
-		if (debugOn) 
+		else 
 		{
-			Debug.Log("Damage Taken! " + "Health = " + health.ToString());
+			deathEvent(); //Died, not implemented yet
 		}
 	}
 
 	public bool gainHealth(int heal) //Boolean could be used to show something in the UI
 	{
-		if (health == maxHealth) 
+		if (health < maxHealth) 
 		{
-			health = maxHealth;
-			return false; //Event: Player has max health
+			health += heal;
+			if (health > maxHealth) 
+			{
+				health = maxHealth; //If health went over, set it back to max
+			}
+			if (debugOn) 
+			{
+				Debug.Log("Health Gained! " + "Health = " + health.ToString());
+			}
+			return true; //Event: Player healed
 		}
-		this.health += heal;
-		if (health > maxHealth) 
-		{
-			health = maxHealth; //Stops at maxHealth
-		}
-		if (debugOn) 
-		{
-			Debug.Log("Health Gained! " + "Health = " + health.ToString());
-		}
-		return true; //Event: Player healed
+		return false; //Event: Player has max health
 	}
 
 	public bool spendBattery(int batteryCost)
 	{
-		if ( battery - batteryCost < 0 ) 
+		int newBattery = battery - batteryCost;
+		if ( newBattery >= 0 ) 
 		{
-			return false; //Dont have enough battery to spend
+			battery = newBattery;
+			if (debugOn) 
+			{
+				Debug.Log("Battery spent! " + "Battery = " + battery.ToString());
+			}
+			return true; //Spent battery
 		}
-		battery -= batteryCost;
-		if (debugOn) 
-		{
-			Debug.Log("Battery spent! " + "Battery = " + battery.ToString());
-		}
-		return true; //Spent Battery
+		return false; //Don't have enough battery
 	}
 
 	public bool gainBattery(int charges)
@@ -75,60 +78,60 @@ public class PlayerStats : MonoBehaviour
 		According to the game design doc, battery only regenerates. This is here
 		Just in case we add consumables that restore battery
 		*/
-		if ( battery == maxBattery ) 
+		if (battery < maxBattery) 
 		{
-			return false; //Player has max battery
+			battery += charges;
+			if (battery > maxBattery) 
+			{
+				battery = maxBattery; 
+			}
+			if (debugOn) 
+			{
+				Debug.Log("Battery Gained! " + "Battery = " + battery.ToString());
+			}
+			return true; //Battery gained successfully
 		}
-		battery += charges;
-		if ( battery > maxBattery ) 
-		{
-			battery = maxBattery;
-		}
-		if (debugOn) 
-		{
-			Debug.Log("Battery Gained! " + "Battery = " + battery.ToString());
-		}
-		return true; //Battery gained successfully
+		return false; //Player has max battery
 	}
 
-	public void upgradeMaxBattery(int addedCharges) //Method upgrades the player's max amount of charges
+	public bool upgradeMaxBattery(int addedCharges) //Method upgrades the player's max amount of charges
 	{
-		if (maxBattery + addedCharges > limitBattery) 
+		if (maxBattery < limitBattery) 
 		{
-			batteryLimitReached();
-			return;
+			maxBattery += addedCharges;
+			if (maxBattery > limitBattery)
+			{
+				maxBattery = limitBattery;
+			}
+			if (debugOn)
+			{
+				Debug.Log("New Battery Limit = " + maxBattery.ToString());
+			}
+			return true; //Battery upgraded successfully
 		}
-		maxBattery += addedCharges;
-		if (debugOn)
-		{
-			Debug.Log("New Battery Limit = " + maxBattery.ToString());
-		}
+		return false; //Cant upgrade battery anymore
 	}
 
-	public bool upgradeMaxHealth(int upgradeNum) {
-
-		if (maxHealth >= limitHealth) 
+	public bool upgradeMaxHealth(int upgradeNum) 
+	{
+		if (maxHealth < limitHealth) 
 		{
-			return false; //Cant upgrade health any further
+			maxHealth += upgradeNum;
+			if (maxHealth > limitHealth)
+			{
+				maxHealth = limitHealth;
+			}
+			if (debugOn) 
+			{
+				Debug.Log("New Health Limit = " + maxHealth.ToString());
+			}
+			return true; //Successfully upgraded Health
 		}
-		if (maxHealth > limitHealth) 
-		{
-			maxHealth = limitHealth; //Allows last upgrade of health
-		}
-		if (debugOn) 
-		{
-			Debug.Log("New Health Limit = " + maxHealth.ToString());
-		}
-		return true; //Successfully upgraded Health
+		return false; //Cant upgrade health any further
 	}
 
 	public void deathEvent() {
 		//Place code here to execute when the player dies
-	}
-
-	public void batteryLimitReached() 
-	{
-		//Method to tell the player that the battery Limit cant be increased further
 	}
 
 	//Getters
