e01fe5fba4fa76f120ee8066493af7caaef043d2
FalconKiaT <ian.fletcher778@gmail.com>
2023-04-07 16:14:36 -0700

    Finished Health and Battery Bars
    - Improved UI assets
    - Now working on crosshair system

---
+++ Assets/Scripts/UI/FletcherTesting/CrosshairRecoil.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CrosshairRecoil : MonoBehaviour
+{
+    /*
+    public Image wingTopRight;
+    public Image wingTopLeft;
+    public Image wingBottomRight;
+    public Image wingBottomLeft;
+    public Vector2 startPos = (20,20); //Possibly make it readonly when ready
+    public int TEMP_recoil = 20;
+    private RectTransform wingUpRight; //Couldnt find better naming scheme fuk
+    private RectTransform wingUpLeft;
+    private RectTransform wingDownRight;
+    private RectTransform wingDownLeft;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //wingTopRight Setup
+        wingUpRight = wingTopRight.GetComponent<RectTransform>();
+        wingUpRight.anchoredPosition = startPos;
+        //wingTopLeft Setup
+        wingUpLeft = wingTopLeft.GetComponent<RectTransform>();
+        wingUpLeft.anchoredPosition = startPos;
+        //wingBottomRight Setup
+        wingDownRight = wingBottomRight.GetComponent<RectTransform>();
+        wingDownRight.anchoredPosition = startPos;
+        //wingBottomLeft Setup
+        wingDownLeft = wingBottomLeft.GetComponent<RectTransform>();
+        wingDownLeft.anchoredPosition = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updatePosition(TEMP_recoil);
+    }
+
+    private void updatePosition(int recoil) 
+    {
+        public Vector2 newPos = (recoil , recoil);
+
+        wingUpRight.anchoredPosition = newPos;
+        wingUpLeft.anchoredPosition = newPos;
+        wingDownRight.anchoredPosition = newPos;
+        wingDownLeft.anchoredPosition = newPos;
+    }
+    */
+}
