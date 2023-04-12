1fb0e2aebc6e8c4e6493a320432f3dc14407290e
FalconKiaT <ian.fletcher778@gmail.com>
2023-04-12 01:08:59 -0700

    cool mafs for crosshair recoil
    Designed a script that will open the crosshair depending on the amount of recoil the weapon has. Now trying to understand how do events work.

---
+++ Assets/Scripts/UI/FletcherTesting/SignalTest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+[CreateAssetMenu]
+public class SignalTest : ScriptableObject
+{
+    private List<SignalListener> listeners = new List<SignalListener>();
+
+    public void Raise() 
+    {
+        //Go backwards on list in case an object deletes itself from list
+        for (int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].onSignalRaised();
+        }
+    }
+
+    public void RegisterListener(SignalListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    public void DeRegisterListener(SignalListener listener) 
+    {
+        listeners.Remove(listener);
+    }
+}
+*/
