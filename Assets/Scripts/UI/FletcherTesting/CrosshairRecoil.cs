98be4a0c4b6996ec5dbe6b2f956c6ebff03a456e
FalconKiaT <ian.fletcher778@gmail.com>
2023-04-12 01:08:59 -0700

    cool mafs for crosshair recoil
    Designed a script that will open the crosshair depending on the amount of recoil the weapon has. Now trying to understand how do events work.

--- Assets/Scripts/UI/FletcherTesting/CrosshairRecoil.cs
+++ Assets/Scripts/UI/FletcherTesting/CrosshairRecoil.cs
@@ -6,49 +6,63 @@ using UnityEngine.UI;
 
 public class CrosshairRecoil : MonoBehaviour
 {
-    /*
+    private float radianAngle = (Mathf.PI / 4.0f); //Store radian angle for better efficiency (45°)
+    //Recoil Vars
+    public float recoilFloor = 9.0f; //minimum distance from center, FIXME: WHEN RECOIL SYSTEM IS FINISHED, MAKE IT READONLY
+    public float recoilTEST;
+    //Wings of the crosshair
     public Image wingTopRight;
     public Image wingTopLeft;
     public Image wingBottomRight;
     public Image wingBottomLeft;
-    public Vector2 startPos = (20,20); //Possibly make it readonly when ready
-    public int TEMP_recoil = 20;
-    private RectTransform wingUpRight; //Couldnt find better naming scheme fuk
+    //Retrieve their RectTransforms, couldnt come up with better names soz :(
+    private RectTransform wingUpRight;
     private RectTransform wingUpLeft;
     private RectTransform wingDownRight;
-    private RectTransform wingDownLeft;
+    private RectTransform wingDownLeft; 
     
     // Start is called before the first frame update
     void Start()
     {
-        //wingTopRight Setup
-        wingUpRight = wingTopRight.GetComponent<RectTransform>();
-        wingUpRight.anchoredPosition = startPos;
-        //wingTopLeft Setup
-        wingUpLeft = wingTopLeft.GetComponent<RectTransform>();
-        wingUpLeft.anchoredPosition = startPos;
-        //wingBottomRight Setup
-        wingDownRight = wingBottomRight.GetComponent<RectTransform>();
-        wingDownRight.anchoredPosition = startPos;
-        //wingBottomLeft Setup
-        wingDownLeft = wingBottomLeft.GetComponent<RectTransform>();
-        wingDownLeft.anchoredPosition = startPos;
+        //Get the RectTransforms
+        this.wingUpRight = wingTopRight.GetComponent<RectTransform>();
+        this.wingUpLeft = wingTopLeft.GetComponent<RectTransform>();
+        this.wingDownRight = wingBottomRight.GetComponent<RectTransform>();
+        this.wingDownLeft = wingBottomLeft.GetComponent<RectTransform>(); 
+        
+        //Set the recoil temporarly
+        recoilTEST = recoilFloor + 0.001f;
+        //Set wings to the minimum amount of radius shift
+        updatePositions(recoilTEST); //number makes sure that the "if" is entered
     }
 
     // Update is called once per frame
     void Update()
     {
-        updatePosition(TEMP_recoil);
+        updatePositions(recoilTEST);
     }
 
-    private void updatePosition(int recoil) 
+    private void updatePositions(float recoil) 
     {
-        public Vector2 newPos = (recoil , recoil);
+        if (recoil > recoilFloor)
+        {
+            float shift = calcShift(recoil);
 
-        wingUpRight.anchoredPosition = newPos;
-        wingUpLeft.anchoredPosition = newPos;
-        wingDownRight.anchoredPosition = newPos;
-        wingDownLeft.anchoredPosition = newPos;
+            //Shift Top Right
+            this.wingUpRight.anchoredPosition = new Vector3(+shift , +shift, 0);
+            //Shift Top Left
+            this.wingUpLeft.anchoredPosition = new Vector3(-shift , +shift, 0);
+            //Shift Bottom Right
+            this.wingDownRight.anchoredPosition = new Vector3(+shift , -shift, 0);
+            //Shift Bottom Left
+            this.wingDownLeft.anchoredPosition = new Vector3(-shift , -shift, 0);
+        }
     }
-    */
+
+    private float calcShift (float radius) 
+    {
+        //Since they are at a 45°, the X and Y shift are the same
+        return radius * Mathf.Cos(this.radianAngle);
+    }
+
 }
