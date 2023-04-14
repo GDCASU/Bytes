7a412339c5250bdd806d9fed08892d9362da201f
FalconKiaT <ian.fletcher778@gmail.com>
2023-04-14 01:21:53 -0700

    Implemented Health Acceleration
    Health bar now shows shadow of health decay to better gauge damage received

--- Assets/Scripts/UI/FletcherTesting/HealthBarScript.cs
+++ Assets/Scripts/UI/FletcherTesting/HealthBarScript.cs
@@ -6,16 +6,70 @@ using UnityEngine.UI;
 public class HealthBarScript : MonoBehaviour
 {
     public Slider healthBar;
+    public Slider accelerationBar;
+    private float accelerationHP;
+    private float currentHP; //This value exists mostly for readability
+    public float accWaitTime = 1f; //Sets how long it takes for acceleration to start. In seconds
+    public float accSpeed = 1f; //How many seconds it takes decay to complete
+    private float accTime; 
     
     void Start()
     {
-        healthBar.maxValue = StatusEvents.player.getMaxHealth();
-        healthBar.value = StatusEvents.player.getHealth();
+        setHealthBar();
+        setAccelerationBar();
+        accelerationHP = healthBar.value;
+        accTime = accWaitTime; //Mutating float for tracking time;
     }
 
     void Update()
     {
+        currentHP = setHealthBar(); //applies damage
+
+        //Code that implements Health Acceleration
+        if (currentHP < accelerationHP) 
+        {
+            //Health was lost, show acceleration
+            if (accTime > 0f) //Waits "accWaitTime" seconds to start lowering the acceleration bar
+            {
+                accTime -= 1f * Time.deltaTime; 
+            }
+            else //After "accWaitTime" seconds have passed, start decay
+            {
+                accelerationBar.value -= (accelerationHP - currentHP) * Time.deltaTime;
+                if (accelerationBar.value <= currentHP) 
+                {
+                    accelerationHP = currentHP;
+                }
+            }
+        }
+        else
+        {
+            //Health stays the same or is gained, set Acceleration bar to the HP bar
+            setAccelerationBar();
+            accTime = accWaitTime;
+            accelerationHP = currentHP;
+        }
+    }
+
+    public void AlertDamage()
+    {
+        //If damaged again, wait "accWaitTime" seconds again
+        accTime = accWaitTime;
+    }
+    
+    private float setHealthBar()
+    {
+        //Sets the health bar, as well as returning its current value;
+        float tempHP = StatusEvents.player.getHealth();
         healthBar.maxValue = StatusEvents.player.getMaxHealth();
-        healthBar.value = StatusEvents.player.getHealth();
+        healthBar.value = tempHP;
+        return tempHP;
+    }
+
+    private void setAccelerationBar()
+    {
+        //Sets the acceleration bar
+        accelerationBar.maxValue = healthBar.maxValue;
+        accelerationBar.value = healthBar.value;
     }
 }
