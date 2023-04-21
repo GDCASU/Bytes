6d6e2b731a05488a1fb80cfd73728094a5cb7438
Aaron Huggins <achuggi1@asu.edu>
2023-04-21 13:39:39 -0700

    Working nav mesh room and enemy
    Implement a working nav mesh room with a single enemy in it. Test with G3 rooms

--- Assets/Scripts/Environment/NavMesh/NavMeshEnemy.cs
+++ Assets/Scripts/Environment/NavMesh/NavMeshEnemy.cs
@@ -5,23 +5,40 @@ using UnityEngine.AI;
 
 public class NavMeshEnemy : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] GameObject target;
+    [SerializeField] NavMeshAgent navAgent;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Enemy Values")]
+    [SerializeField] float damage;
+
+    /*  INTERNALS   */
+    private float timer = .2f;
+    private float stopwatch = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+        stopwatch += Time.deltaTime;
+
+        if (stopwatch >= timer)
+        {
+            navAgent.SetDestination(target.transform.position);
+            stopwatch -= timer;
+        }
     }
 
     public void SetTarget(GameObject newTarget)
     {
-        Debug.Log("Target Set.");
         target = newTarget;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        PlayerStats player;
+        if (player = collision.gameObject.GetComponentInParent<PlayerStats>())
+        {
+            player.recieveDamage(damage);
+        }
     }
 }
